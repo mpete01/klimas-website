4fd95f02cfc2a7159c3744fcb1a86e93b737777a
Molnár Péter Levente <peter.molnar3@2connect.hu>
2025-12-10 23:05:56 +0100

    Started header

---
+++ app/components/Header.jsx
@@ -0,0 +1,24 @@
+//font - #2174A0
+import Image from "next/image";
+
+function Header(){
+
+    return(
+        <header>
+            <div className="w-full flex items-center justify-between p-2">
+                <div>
+                    <Image src="/logo-nobg.png" alt="company logo" width={150} height={100} className="ml-32"/>
+                </div>
+                <div className="flex space-x-40 mr-16">
+                    <button className="px-4 py-2 bg-[#2174A0] text-[rgb(219,219,219)]">Home</button>
+                    <button className="px-4 py-2 bg-blue-500 text-white rounded">About</button>
+                    <button className="px-4 py-2 bg-blue-500 text-white rounded">Services</button>
+                    <button className="px-4 py-2 bg-blue-500 text-white rounded">Contact</button>
+                </div>
+            </div>
+            <div className="bg-[#2174A0] h-1 w-full mt-1"></div>
+        </header>
+    )
+}
+
+export default Header
---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Header from "./components/Header";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-[rgb(219,219,219)] font-sans">
+      <Header />
+      
+    </div>
+  );
+}
